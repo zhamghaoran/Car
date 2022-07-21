6c76673a2a48c76040f0af300cc8a1ec1bc57e52
justacp <justacp@163.com>
2022-07-21 11:13:20 +0800

    再次提交

--- anonymous/car/car_privatecar.sql
+++ anonymous/car/car_privatecar.sql
@@ -1,6 +1,6 @@
 create table car.car_privatecar
 (
-    Id     int auto_increment
+    id     int auto_increment
         primary key,
     brand  varchar(20) null,
     price  int         null,
--- anonymous/car/car_truck.sql
+++ anonymous/car/car_truck.sql
@@ -1,6 +1,6 @@
 create table car.car_truck
 (
-    Id     int auto_increment
+    id     int auto_increment
         primary key,
     brand  varchar(20) null,
     `load` int         null,
